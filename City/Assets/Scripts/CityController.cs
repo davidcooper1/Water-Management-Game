dc7c1fbaa5074d5049ea131c25fa0743090de5a4
davidcooper1 <davidpcooper1@gmail.com>
2018-04-10 22:40:07 -0500

    Uploaded city files with controller script.

---
+++ City/Assets/Scripts/CityController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityController : MonoBehaviour {
+
+    public GameObject Lake;
+    public GameObject RainParticles;
+    public GameObject RainCloud;
+    private float lakeMax;
+    private float lakeCurrent;
+    private bool lakeUpdateNeeded;
+
+	// Use this for initialization
+	void Start () {
+        SetLakeMax(100);
+        SetLakeCurrent(100);
+        SetRainEnabled(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lakeUpdateNeeded) {
+            Vector3 oldLakePosition = Lake.transform.position;
+            Lake.transform.position = new Vector3(oldLakePosition.x, 6 + 8 * (lakeCurrent / lakeMax), oldLakePosition.z);
+            lakeUpdateNeeded = false;
+        }
+	}
+
+    public void SetLakeMax(float newMax) {
+        lakeMax = newMax;
+        lakeUpdateNeeded = true;
+    }
+
+    public void SetLakeCurrent(float newCurrent) {
+        lakeCurrent = newCurrent;
+        lakeUpdateNeeded = true;
+    }
+
+    public void SetRainEnabled(bool rainEnabled) {
+        RainParticles.SetActive(rainEnabled);
+        RainCloud.SetActive(rainEnabled);
+    }
+}
